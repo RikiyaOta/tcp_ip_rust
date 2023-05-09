fc1b6c18b14cb8c44775b4d7587970ceceffc1b8
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-05-09 22:59:52 +0900

    Add some getter/setters. To be continue.

--- src/internet_protocol.rs
+++ src/internet_protocol.rs
@@ -1,5 +1,3 @@
-use byteorder::{ByteOrder, NetworkEndian};
-
 /*
  * 注意：IHL (Internet Header Length) 自体は 4bits.
  * しかし、それは 32bit words つまり 4bytes 単位でデータを表現している。なぜなら、IPヘッダーは各フィールドが32bitsだから。
@@ -33,8 +31,17 @@ pub struct Ipv4Header {
 
     total_length: u16,
     identification: u16,
+
+    /*
+     * 注意：Flags は 3bits
+     */
     flags: u8,
+
+    /*
+     * 注意：Fragment Offset は　13bits
+     */
     fragment_offset: u16,
+
     ttl: u8,
     protocol: u8,
     checksum: u16,
@@ -56,7 +63,7 @@ impl Ipv4Header {
         self.version = version;
     }
 
-    pub fn get_version(&mut self) -> u8 {
+    pub fn get_version(&self) -> u8 {
         /*
          * 補足：`&`はビット単位の論理積(AND)
          *
@@ -75,5 +82,69 @@ impl Ipv4Header {
 
     pub fn get_ihl(&self) -> u8 {
         self.ihl & 0xF
+    }
+
+    pub fn get_dscp_ecn(&self) -> u8 {
+        let dscp = self.dscp & 0b0011_1111; // 上位 6bits を表す。
+        let ecn = self.ecn & 0b0000_0011; // 下位 2bits を表す。
+
+        (dscp << 2) | ecn
+    }
+
+    pub fn get_total_length(&self) -> u16 {
+        self.total_length
+    }
+
+    pub fn get_identification(&self) -> u16 {
+        self.identification
+    }
+
+    pub fn get_flags(&self) -> u8 {
+        self.flags & 0b0000_0111
+    }
+
+    pub fn set_flags(&mut self, flags: u8) {
+        assert!(
+            flags <= 0b0000_0111,
+            "Invalid Flags value!!! It should be 3bits value"
+        );
+        self.flags = flags;
+    }
+
+    pub fn get_fragment_offset(&self) -> u16 {
+        // これと同じ：self.fragment_offset & 0b0001_1111_1111_1111
+        self.fragment_offset & ((1 << 13) - 1)
+    }
+
+    pub fn encode(&self) -> Vec<u8> {
+        let mut buffer = vec![0u8; 20];
+
+        /*
+         * Version and IHL
+         */
+        buffer[0] = (self.get_version() << 4) | self.get_ihl();
+
+        /*
+         * Type of Service
+         */
+        buffer[1] = self.get_dscp_ecn();
+
+        /*
+         * Total Length
+         */
+        buffer[2..4].copy_from_slice(&self.get_total_length().to_be_bytes());
+
+        /*
+         * Identification
+         */
+        buffer[4..6].copy_from_slice(&self.get_identification().to_be_bytes());
+
+        /*
+         * Flags and Fragment Offset
+         */
+        let flags_fragment_offset = ((self.get_flags() as u16) << 13) | self.get_fragment_offset();
+        buffer[6..8].copy_from_slice(&flags_fragment_offset.to_be_bytes());
+
+        todo!();
     }
 }
