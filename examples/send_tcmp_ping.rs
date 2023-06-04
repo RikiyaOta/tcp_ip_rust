b99ab35444c636c5d7a88d0a8f206d1f29e821f0
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-06-04 10:17:06 +0900

    Fix a little.

--- examples/send_tcmp_ping.rs
+++ examples/send_tcmp_ping.rs
@@ -1,10 +1,11 @@
-use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
-use pnet::packet::icmp::IcmpTypes;
+use pnet::packet::icmp::echo_request::{IcmpCodes, MutableEchoRequestPacket};
+use pnet::packet::icmp::{time_exceeded, IcmpPacket, IcmpType, IcmpTypes};
 use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::ipv4::MutableIpv4Packet;
-use pnet::packet::Packet;
-use pnet::transport::icmp_packet_iter;
+use pnet::packet::ipv4::{Ipv4Packet, MutableIpv4Packet};
+use pnet::packet::{MutablePacket, Packet};
+use pnet::transport::{icmp_packet_iter, ipv4_packet_iter};
 use pnet::transport::{transport_channel, TransportChannelType::Layer3};
+use std::net::IpAddr;
 use std::net::Ipv4Addr;
 use std::process;
 
@@ -17,30 +18,29 @@ const HEADER_LEN: usize = IPV4_HEADER_LEN + ICMP_HEADER_LEN;
  * Google の　DNS サーバーにリクエストを投げてみる
  */
 fn main() {
-    let icmp_type = IcmpTypes::EchoRequest;
     let destination = Ipv4Addr::new(8, 8, 8, 8);
-    let protocol = Layer3(IpNextHeaderProtocols::Icmp);
-
-    let (mut tx, mut rx) = transport_channel(4096, protocol).unwrap();
-
-    let mut ipv4_buffer = [0u8; IPV4_HEADER_LEN + ICMP_HEADER_LEN + ICMP_PAYLOAD_LEN];
-    let mut icmp_buffer = [0u8; ICMP_HEADER_LEN + ICMP_PAYLOAD_LEN];
 
     /*
      * Construct IPv4 Packet.
      */
+    let mut ipv4_buffer = [0u8; IPV4_HEADER_LEN + ICMP_HEADER_LEN + ICMP_PAYLOAD_LEN];
     let mut ipv4_packet = MutableIpv4Packet::new(&mut ipv4_buffer).unwrap();
     ipv4_packet.set_version(4);
     ipv4_packet.set_header_length((IPV4_HEADER_LEN as u8) / 4);
-    ipv4_packet.set_total_length((HEADER_LEN + ICMP_PAYLOAD_LEN) as u16);
+    //ipv4_packet.set_total_length((HEADER_LEN + ICMP_PAYLOAD_LEN) as u16);
+    ipv4_packet.set_total_length(HEADER_LEN as u16);
+    ipv4_packet.set_identification(1);
     ipv4_packet.set_next_level_protocol(IpNextHeaderProtocols::Icmp);
     ipv4_packet.set_destination(destination);
 
     /*
      * Construct ICMP Packet.
      */
+    let mut icmp_buffer = [0u8; ICMP_HEADER_LEN + ICMP_PAYLOAD_LEN];
     let mut icmp_packet = MutableEchoRequestPacket::new(&mut icmp_buffer).unwrap();
-    icmp_packet.set_icmp_type(icmp_type);
+    icmp_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    icmp_packet.set_icmp_code(IcmpCodes::NoCode);
+    icmp_packet.set_checksum(0);
     icmp_packet.set_sequence_number(1);
     icmp_packet.set_identifier(1);
 
@@ -49,36 +49,100 @@ fn main() {
      */
     let checksum = pnet::util::checksum(icmp_packet.packet(), 1);
     icmp_packet.set_checksum(checksum);
-    ipv4_packet.set_payload(icmp_packet.packet());
-
-    tx.send_to(ipv4_packet, destination.into()).unwrap();
-
-    let mut iter = icmp_packet_iter(&mut rx);
-    
-    println!("Before loop.");
+    ipv4_packet.set_payload(&icmp_packet.packet());
 
     /*
-     * Listen for replies.
+     * Send request.
      */
-    loop {
-        println!("First statement in loop.");
-        match iter.next() {
-            Ok((packet, addr)) => {
-                if addr == destination {
-                    if packet.get_icmp_type() == IcmpTypes::EchoReply {
-                        println!("Received reply from {}", addr);
-                        break;
-                    } else {
-                        println!("[packet.get_icmp_type() != IcmpTypes::EchoReply] icmp_type={:?}", packet.get_icmp_type());
+
+    let (mut tx, mut rx) = transport_channel(512, Layer3(IpNextHeaderProtocols::Icmp)).unwrap();
+    let mut rx = ipv4_packet_iter(&mut rx);
+
+    let mut reach = 0;
+
+    println!("[*] Scaning route => {}", ipv4_packet.get_destination());
+
+    for i in 1..255 {
+        ipv4_packet.set_ttl(i);
+        let ipv4 = Ipv4Packet::new(&ipv4_packet.packet()).unwrap();
+        tx.send_to(ipv4, IpAddr::V4(ipv4_packet.get_destination())).unwrap();
+
+        for _ in 0..3 {
+            match rx.next() {
+                Ok((packet, _addr)) => {
+                    let i_pac = Ipv4Packet::new(packet.packet()).unwrap();
+                    match i_pac.get_next_level_protocol() {
+                        IpNextHeaderProtocols::Icmp => {
+                            let icmp_pac = IcmpPacket::new(i_pac.payload()).unwrap();
+                            match icmp_pac.get_icmp_type() {
+                                IcmpTypes::EchoReply => {
+                                    reach = 1;
+                                    println!("[*] Reach ttl: {} from {}", i, i_pac.get_source());
+                                    break;
+                                }
+                                IcmpTypes::TimeExceeded => match icmp_packet.get_icmp_code() {
+                                    time_exceeded::IcmpCodes::TimeToLiveExceededInTransit => {
+                                        reach = 0;
+                                        println!("- ttl: {} from {}", i, i_pac.get_source());
+                                        break;
+                                    }
+                                    _ => {
+                                        println!("[*] IcmpType:TimeExceeded unknown response");
+                                    }
+                                },
+                                _ => {
+                                    println!(".");
+                                }
+                            }
+                        }
+                        _ => {
+                            reach = 2;
+                            println!(".");
+                        }
                     }
-                } else {
-                    println!("[addr != destination] addr={}. packet={:?}", addr, packet);
                 }
-            },
-            Err(error) => {
-                eprintln!("Error receiving packet: {}", error);
-                process::exit(1);
+                Err(_) => {
+                    println!(".")
+                }
             }
-        }    
+        }
+
+        if reach == 1 {
+            break;
+        }
     }
+
+    //tx.send_to(ipv4_packet, destination.into()).unwrap();
+
+    //let mut iter = icmp_packet_iter(&mut rx);
+
+    //println!("Before loop.");
+
+    // /*
+    // * Listen for replies.
+    // */
+    //loop {
+    //    println!("First statement in loop.");
+    //    match iter.next() {
+    //        Ok((packet, addr)) => {
+    //            if addr == destination {
+    //                if packet.get_icmp_type() == IcmpTypes::EchoReply {
+    //                    println!("Received reply from {}", addr);
+    //                    break;
+    //                } else {
+    //                    println!(
+    //                        "[packet.get_icmp_type() != IcmpTypes::EchoReply] icmp_type={:?}",
+    //                        packet.get_icmp_type()
+    //                    );
+    //                }
+    //            } else {
+    //                println!("[addr != destination] addr={}. packet={:?}", addr, packet);
+    //            }
+    //        }
+    //        Err(error) => {
+    //            eprintln!("Error receiving packet: {}", error);
+    //            process::exit(1);
+    //        }
+    //    }
+    //}
 }
